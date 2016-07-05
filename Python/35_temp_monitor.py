60440ea3613268547aa9ba4ffa6fe828bb7c4a5d
Marcelo Bernardes <bernardes.marcelo@gmail.com>
2016-07-05 12:56:26 -0600

    Update 35_temp_monitor.py
    Added various comments to the code.
    
    Renamed edge "temp" variable to the more descriptive name "action".
    
    Fixed the logic for changing the low and right thresholds.
    
    Clarified the print statements to indicate temperature is displayed in Celsius.
    
    Adjusted if conditions on lines 65 and 68 to consistently evaluate current temperature outside of thresholds.

--- Python/35_temp_monitor.py
+++ Python/35_temp_monitor.py
@@ -1,4 +1,4 @@
-import RPi.GPIO as  GPIO
+import RPi.GPIO as GPIO
 import importlib
 import time
 import sys
@@ -15,54 +15,70 @@ Buzz	=	15
 joystick	=	importlib.import_module('15_joystick_PS2')
 ds18b20		=	importlib.import_module('26_ds18b20')
 beep		=	importlib.import_module('10_active_buzzer')
-rgb			=	importlib.import_module('02_rgb_led')
+rgb		=	importlib.import_module('02_rgb_led')
 
 joystick.setup()
 ds18b20.setup()
 beep.setup(Buzz)
 rgb.setup(LedR, LedG, LedB)
 
+# Define LED colors for later use
 color = {'Red':0xFF0000, 'Green':0x00FF00, 'Blue':0x0000FF}
 
 def setup():
 	global lowl, highl
+	# Initialize the low and high temperature thresholds
 	lowl = 29
 	highl = 31
 
 def edge():
+	# Adjust temperature threshholds by using the joystick
 	global lowl, highl
-	temp = joystick.direction()
-	if temp == 'Pressed':
+	action = joystick.direction()
+	# Pressing the joystick button caused the code to terminate
+	if action == 'Pressed':
 		destroy()
 		quit()
-	if temp == 'up' and lowl < highl-1:
+	# Up/Down decrease/increase the high temperature threshold
+	if action == 'up' and highl <= 125:
 		highl += 1
-	if temp == 'down' and lowl >= -5:
+	if action == 'down' and lowl < highl-1:
 		highl -= 1
-	if temp == 'right' and highl <= 125:
+	# Left/Right decrease/increase the low temperature threshold
+	if action == 'right' and lowl < highl-1:
 		lowl += 1
-	if temp == 'left' and lowl < highl-1:
+	if action == 'left' and lowl >= -5:
 		lowl -= 1
 
 def loop():
 	while True:
 		edge()
 		temp = ds18b20.read()
-		print 'The lower limit of temperature : ', lowl
-		print 'The upper limit of temperature : ', highl
-		print 'Current temperature : ', temp
+		# Display the low, high and current temperatures in Celsius
+		print 'The lower limit of temperature : %0.3f C', % lowl
+		print 'The upper limit of temperature : %0.3f C', % highl
+		print 'Current temperature : %0.3f C', % temp
+		# Check to see if lower or upper temperature threshholds have been breached
+		# Color code LED and play buzzer tone as follows:
+		# - below lower threshhold: blue LED, buzzer sounds 3 times
+		# - within limits: green LED, buzzer silent
+		# - above upper threshhold: red LED, buzzer sounds 3 times
 		if float(temp) < float(lowl):
+			# Temperature is below lower threshold
 			rgb.setColor(color['Blue'])
 			for i in range(0, 3):
 				beep.beep(0.5)
-		if temp >= float(lowl) and temp < float(highl):
+		if temp >= float(lowl) and temp <= float(highl):
+			# Temperature is within thresholds
 			rgb.setColor(color['Green'])
-		if temp >= float(highl):
+		if temp > float(highl):
+			# Temperature is above higher threshold
 			rgb.setColor(color['Red'])
 			for i in range(0, 3):
 				beep.beep(0.1)
 
 def destroy():
+	# Clean up before exiting
 	beep.destroy()
 	joystick.destroy()
 	ds18b20.destroy()
